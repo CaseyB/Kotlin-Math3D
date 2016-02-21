d9eeb8d93709f33796a074ecce3082e8ac3d9525
Casey Borders <TheBeast.13@gmail.com>
2016-02-21 17:17:45 -0500

    Add documentation

--- src/productions/moo/kotlin/math/Math.kt
+++ src/productions/moo/kotlin/math/Math.kt
@@ -2,8 +2,30 @@
 
 package productions.moo.kotlin.math
 
+/**
+ * Clamps the value between min and max
+ *
+ * @return Returns a Float whose value is determined as follows:
+ *   * if the value is less than min, min is returned
+ *   * if the value is greater then max, max is returned
+ *   * otherwise the value is unchanged
+ */
 fun Float.clamp (min: Float, max: Float) = Math.min(Math.max(this, min), max)
 
+/**
+ * Represents an angle
+ *
+ * Handles conversion between radians and degrees
+ *
+ * @param radians Optional value of the angle in radians
+ * @param degrees Optional value of the angle in degrees
+ *
+ * @constructor Takes the measure of the angle in either radians or degrees. If neither
+ * value is supplied the angle will be initialized to 0
+ *
+ * @property radians Get or set the value of the angle in radians
+ * @property degrees Get or set the value of the angle in degrees
+ */
 class Angle(radians: Float? = null, degrees: Float? = null)
 {
     var radians: Float = 0f
