14fbb6bf7170f47714d994f89716eae15e57a1af
Casey Borders <TheBeast.13@gmail.com>
2016-02-21 15:00:12 -0500

    Add Vector2

--- src/Math3D.kt
+++ src/Math3D.kt
@@ -2,6 +2,11 @@
 operator fun Float.times (vector: Vector3) = Vector3(this * vector.x, this * vector.y, this * vector.z)
 operator fun Float.div (vector: Vector3) = Vector3(this / vector.x, this / vector.y, this / vector.z)
 
+// Override operators to work with Vector2
+operator fun Float.times (vector: Vector2) = Vector2(this * vector.x, this * vector.y)
+operator fun Float.div (vector: Vector2) = Vector2(this / vector.x, this / vector.y)
+
+// region Vector3
 data class Vector3 (var x: Float, var y: Float, var z: Float)
 {
     // Artihmetic operations with other Vector3s
@@ -69,3 +74,66 @@ data class Vector3 (var x: Float, var y: Float, var z: Float)
         fun distanceSquaredFrom (vectors: Pair<Vector3, Vector3>) = (vectors.first - vectors.second).lengthSquared
     }
 }
+// endregion
+
+// region Vector2
+data class Vector2 (var x: Float, var y: Float)
+{
+    // Artihmetic operations with other Vector2s
+    operator fun plus  (other: Vector2) = Vector2(x + other.x, y + other.y)
+    operator fun minus (other: Vector2) = Vector2(x - other.x, y - other.y)
+    operator fun times (other: Vector2) = Vector2(x * other.x, y * other.y)
+    operator fun div   (other: Vector2) = Vector2(x / other.x, y / other.y)
+
+    // Unary operations
+    operator fun unaryPlus  () = this
+    operator fun unaryMinus () = Vector2(-x, -y)
+
+    // Artihmetic operations with a float scalar
+    operator fun times (scalar: Float) = Vector2(x * scalar, y * scalar)
+    operator fun div   (scalar: Float) = Vector2(x / scalar, y / scalar)
+
+    val length: Float
+        get() = Math.sqrt((lengthSquared).toDouble()).toFloat()
+
+    val lengthSquared: Float
+        get() = x * x + y * y
+
+    fun distanceFrom (other: Vector2) = (this - other).length
+    fun distanceSquaredFrom (other: Vector2) = (this - other).lengthSquared
+
+    fun normalize (): Float
+    {
+        val oldLength = length
+
+        if (oldLength > 0)
+        {
+            val lengthInverse = 1.0f / oldLength
+            x *= lengthInverse
+            y *= lengthInverse
+        }
+
+        return oldLength
+    }
+
+    infix fun dot (other: Vector2) = x * other.x + y * other.y
+
+    companion object
+    {
+        val ZERO = Vector2(0f, 0f)
+
+        val UNIT_X = Vector2(1f, 0f)
+        val UNIT_Y = Vector2(0f, 1f)
+        val UNIT_Z = Vector2(0f, 0f)
+
+        val NEGATIVE_UNIT_X = Vector2(-1f, 0f)
+        val NEGATIVE_UNIT_Y = Vector2(0f, -1f)
+        val NEGATIVE_UNIT_Z = Vector2(0f, 0f)
+
+        val UNIT_SCALE = Vector2(1f, 1f)
+
+        fun distanceFrom (vectors: Pair<Vector2, Vector2>) = (vectors.first - vectors.second).length
+        fun distanceSquaredFrom (vectors: Pair<Vector2, Vector2>) = (vectors.first - vectors.second).lengthSquared
+    }
+}
+// endregion
