5ce8a63a4825997ba8a785f710a5cf365471696c
Casey Borders <borders@beamtoothbrush.com>
2016-03-18 16:29:19 -0400

    Make vector buffers

--- src/productions/moo/kotlin/math3d/Vector.kt
+++ src/productions/moo/kotlin/math3d/Vector.kt
@@ -4,39 +4,68 @@ package productions.moo.kotlin.math3d
 
 import productions.moo.kotlin.math.Angle
 import productions.moo.kotlin.math.clamp
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 
 // region Float extensions
 // Override operators to work with Vector3
-operator fun Float.times (vector: Vector3) = Vector3(this * vector.x, this * vector.y, this * vector.z)
-operator fun Float.div (vector: Vector3) = Vector3(this / vector.x, this / vector.y, this / vector.z)
+operator fun Float.times (vector: Vector3) = Vector3(this * vector.buffer.get(0), this * vector.buffer.get(1), this * vector.buffer.get(2))
+operator fun Float.div (vector: Vector3) = Vector3(this / vector.buffer.get(0), this / vector.buffer.get(1), this / vector.buffer.get(2))
 
 // Override operators to work with Vector2
-operator fun Float.times (vector: Vector2) = Vector2(this * vector.x, this * vector.y)
-operator fun Float.div (vector: Vector2) = Vector2(this / vector.x, this / vector.y)
+operator fun Float.times (vector: Vector2) = Vector2(this * vector.buffer.get(0), this * vector.buffer.get(1))
+operator fun Float.div (vector: Vector2) = Vector2(this / vector.buffer.get(0), this / vector.buffer.get(1))
 // endregion
 
 // region Vector3
-data class Vector3 (var x: Float, var y: Float, var z: Float)
+class Vector3
 {
+    internal val buffer: FloatBuffer = ByteBuffer.allocateDirect(3 * 4).order(ByteOrder.nativeOrder()).asFloatBuffer()
+
+    constructor()
+    {
+        buffer.put(floatArrayOf(0f, 0f, 0f))
+        buffer.rewind()
+    }
+
+    constructor(x: Float, y: Float, z: Float)
+    {
+        buffer.put(floatArrayOf(x, y, z))
+        buffer.rewind()
+    }
+    
+    var x: Float
+        get() { return buffer.get(0) }
+        set(value) { buffer.put(0, value) }
+
+    var y: Float
+        get() { return buffer.get(1) }
+        set(value) { buffer.put(1, value) }
+
+    var z: Float
+        get() { return buffer.get(2) }
+        set(value) { buffer.put(2, value) }
+
     // Artihmetic operations with other Vector3s
-    operator fun plus  (other: Vector3) = Vector3(x + other.x, y + other.y, z + other.z)
-    operator fun minus (other: Vector3) = Vector3(x - other.x, y - other.y, z - other.z)
-    operator fun times (other: Vector3) = Vector3(x * other.x, y * other.y, z * other.z)
-    operator fun div   (other: Vector3) = Vector3(x / other.x, y / other.y, z / other.z)
+    operator fun plus  (other: Vector3) = Vector3(buffer.get(0) + other.buffer.get(0), buffer.get(1) + other.buffer.get(1), buffer.get(2) + other.buffer.get(2))
+    operator fun minus (other: Vector3) = Vector3(buffer.get(0) - other.buffer.get(0), buffer.get(1) - other.buffer.get(1), buffer.get(2) - other.buffer.get(2))
+    operator fun times (other: Vector3) = Vector3(buffer.get(0) * other.buffer.get(0), buffer.get(1) * other.buffer.get(1), buffer.get(2) * other.buffer.get(2))
+    operator fun div   (other: Vector3) = Vector3(buffer.get(0) / other.buffer.get(0), buffer.get(1) / other.buffer.get(1), buffer.get(2) / other.buffer.get(2))
 
     // Unary operations
     operator fun unaryPlus  () = this
-    operator fun unaryMinus () = Vector3(-x, -y, -z)
+    operator fun unaryMinus () = Vector3(-buffer.get(0), -buffer.get(1), -buffer.get(2))
 
     // Artihmetic operations with a float scalar
-    operator fun times (scalar: Float) = Vector3(x * scalar, y * scalar, z * scalar)
-    operator fun div   (scalar: Float) = Vector3(x / scalar, y / scalar, z / scalar)
+    operator fun times (scalar: Float) = Vector3(buffer.get(0) * scalar, buffer.get(1) * scalar, buffer.get(2) * scalar)
+    operator fun div   (scalar: Float) = Vector3(buffer.get(0) / scalar, buffer.get(1) / scalar, buffer.get(2) / scalar)
 
     val length: Float
         get() = Math.sqrt((lengthSquared).toDouble()).toFloat()
 
     val lengthSquared: Float
-        get() = x * x + y * y + z * z
+        get() = buffer.get(0) * buffer.get(0) + buffer.get(1) * buffer.get(1) + buffer.get(2) * buffer.get(2)
 
     fun distanceFrom (other: Vector3) = (this - other).length
     fun distanceSquaredFrom (other: Vector3) = (this - other).lengthSquared
@@ -48,21 +77,21 @@ data class Vector3 (var x: Float, var y: Float, var z: Float)
         if (oldLength > 0)
         {
             val lengthInverse = 1.0f / oldLength
-            x *= lengthInverse
-            y *= lengthInverse
-            z *= lengthInverse
+            buffer.put(0, buffer.get(0) * lengthInverse)
+            buffer.put(1, buffer.get(1) * lengthInverse)
+            buffer.put(2, buffer.get(2) * lengthInverse)
         }
 
         return oldLength
     }
 
-    infix fun dot (other: Vector3) = x * other.x + y * other.y + z * other.z
+    infix fun dot (other: Vector3) = buffer.get(0) * other.buffer.get(0) + buffer.get(1) * other.buffer.get(1) + buffer.get(2) * other.buffer.get(2)
     infix fun cross (other: Vector3): Vector3
     {
         return Vector3(
-                y * other.z - z * other.y,
-                z * other.x - x * other.z,
-                x * other.y - y * other.x)
+                buffer.get(1) * other.buffer.get(2) - buffer.get(2) * other.buffer.get(1),
+                buffer.get(2) * other.buffer.get(0) - buffer.get(0) * other.buffer.get(2),
+                buffer.get(0) * other.buffer.get(1) - buffer.get(1) * other.buffer.get(0))
     }
 
     fun angleTo (other: Vector3): Angle
@@ -82,27 +111,49 @@ data class Vector3 (var x: Float, var y: Float, var z: Float)
 // endregion
 
 // region Vector2
-data class Vector2 (var x: Float, var y: Float)
+class Vector2
 {
+	internal val buffer: FloatBuffer = ByteBuffer.allocateDirect(2 * 4).order(ByteOrder.nativeOrder()).asFloatBuffer()
+
+	constructor()
+	{
+		buffer.put(floatArrayOf(0f, 0f))
+		buffer.rewind()
+	}
+
+	constructor(x: Float, y: Float)
+	{
+		buffer.put(floatArrayOf(x, y))
+		buffer.rewind()
+	}
+
+	var x: Float
+		get() { return buffer.get(0) }
+		set(value) { buffer.put(0, value) }
+
+	var y: Float
+		get() { return buffer.get(1) }
+		set(value) { buffer.put(1, value) }
+	
     // Artihmetic operations with other Vector2s
-    operator fun plus  (other: Vector2) = Vector2(x + other.x, y + other.y)
-    operator fun minus (other: Vector2) = Vector2(x - other.x, y - other.y)
-    operator fun times (other: Vector2) = Vector2(x * other.x, y * other.y)
-    operator fun div   (other: Vector2) = Vector2(x / other.x, y / other.y)
+    operator fun plus  (other: Vector2) = Vector2(buffer.get(0) + other.buffer.get(0), buffer.get(1) + other.buffer.get(1))
+    operator fun minus (other: Vector2) = Vector2(buffer.get(0) - other.buffer.get(0), buffer.get(1) - other.buffer.get(1))
+    operator fun times (other: Vector2) = Vector2(buffer.get(0) * other.buffer.get(0), buffer.get(1) * other.buffer.get(1))
+    operator fun div   (other: Vector2) = Vector2(buffer.get(0) / other.buffer.get(0), buffer.get(1) / other.buffer.get(1))
 
     // Unary operations
     operator fun unaryPlus  () = this
-    operator fun unaryMinus () = Vector2(-x, -y)
+    operator fun unaryMinus () = Vector2(-buffer.get(0), -buffer.get(1))
 
     // Artihmetic operations with a float scalar
-    operator fun times (scalar: Float) = Vector2(x * scalar, y * scalar)
-    operator fun div   (scalar: Float) = Vector2(x / scalar, y / scalar)
+    operator fun times (scalar: Float) = Vector2(buffer.get(0) * scalar, buffer.get(1) * scalar)
+    operator fun div   (scalar: Float) = Vector2(buffer.get(0) / scalar, buffer.get(1) / scalar)
 
     val length: Float
         get() = Math.sqrt((lengthSquared).toDouble()).toFloat()
 
     val lengthSquared: Float
-        get() = x * x + y * y
+        get() = buffer.get(0) * buffer.get(0) + buffer.get(1) * buffer.get(1)
 
     fun distanceFrom (other: Vector2) = (this - other).length
     fun distanceSquaredFrom (other: Vector2) = (this - other).lengthSquared
@@ -114,14 +165,14 @@ data class Vector2 (var x: Float, var y: Float)
         if (oldLength > 0)
         {
             val lengthInverse = 1.0f / oldLength
-            x *= lengthInverse
-            y *= lengthInverse
+            buffer.put(0, buffer.get(0) * lengthInverse)
+            buffer.put(1, buffer.get(1) * lengthInverse)
         }
 
         return oldLength
     }
 
-    infix fun dot (other: Vector2) = x * other.x + y * other.y
+    infix fun dot (other: Vector2) = buffer.get(0) * other.buffer.get(0) + buffer.get(1) * other.buffer.get(1)
 
     companion object
     {
